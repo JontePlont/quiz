009ecc174c2305afbaa9ff6d194a89c29f88b2ac
JontePlont <44798527+JontePlont@users.noreply.github.com>
2019-03-01 15:12:32 +0100

    Add files via upload

--- quiz/js/script.js
+++ quiz/js/script.js
@@ -29,90 +29,170 @@ function rätta(){
 
   if (fråga1 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f1');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f1');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f1');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f1');
     wrong.classList.add('wrong');
   }
 
   if (fråga2 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f2');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f2');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f2');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f2');
     wrong.classList.add('wrong');
   }
 
   if (fråga3 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f3');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f3');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f3');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f3');
     wrong.classList.add('wrong');
   }
 
   if (fråga4 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f4');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f4');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f4');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f4');
     wrong.classList.add('wrong');
   }
 
   if (fråga5 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f5');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f5');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f5');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f5');
     wrong.classList.add('wrong');
   }
 
   if (fråga6 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f6');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f6');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f6');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f6');
     wrong.classList.add('wrong');
   }
 
   if (fråga7 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f7');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f7');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f7');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f7');
     wrong.classList.add('wrong');
   }
 
   if (fråga8 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f8');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f8');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f8');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f8');
     wrong.classList.add('wrong');
   }
 
   if (fråga9 == "Waluigi"){
     rätt++;
+
     var fix = document.getElementById('f9');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f9');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f9');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f9');
     wrong.classList.add('wrong');
   }
 
   if (fråga10 == "Frippe"){
     rätt++;
+
     var fix = document.getElementById('f10');
     fix.classList.remove('wrong');
+
+    var correct = document.getElementById('f10');
+    correct.classList.add('correct');
+
   } else{
+    var fix = document.getElementById('f10');
+    fix.classList.remove('correct');
+
     var wrong = document.getElementById('f10');
     wrong.classList.add('wrong');
   }
@@ -136,7 +216,7 @@ function rätta(){
 
   document.getElementById('resultat').style.display="block";
   document.getElementById('antal_rätt').innerHTML = "Du fick " + rätt + " rätt utav 10!";
-  document.getElementById('bedöm').innerHTML = bedömningar[olikaa];
+  document.getElementById('bedöm').innerHTML = bedömningar[olika];
 
 
 }
